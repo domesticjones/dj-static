37991195508e92a221fe8161dd076e0ae057ad88
Dustin Jones <info@domesticjones.com>
2018-05-01 12:49:29 -0600

    Create About section for desktop

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   // Main Navigation
-  $('.nav-item').on('click', (e) => {
+  $('.nav-item, .inline-link').on('click', (e) => {
     e.preventDefault();
     const $this = $(e.currentTarget);
     const target = $this.attr('href').substring(1);
@@ -14,7 +14,7 @@ $(document).ready(() => {
       $(`.panel[data-section-name="${target}"]`).addClass('is-active');
       $('#container').addClass(`active-${target}`);
       $('.nav-item').removeClass('is-active');
-      $this.addClass('is-active');
+      $(`.nav-item[href="#${target}"]`).addClass('is-active');
     }
   });
 });
