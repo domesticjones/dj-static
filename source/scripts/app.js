7e6316a1b272229d6f1d4cf3b5d7b650769874bb
Dustin Jones <info@domesticjones.com>
2018-05-10 16:10:04 -0600

    Complete Works section

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -46,6 +46,71 @@ $(document).ready(() => {
     $('#contact-check').slideDown();
     $('#contact-pass').slideUp();
   });
+
+  // Works navigation
+  $('.works-nav-button').on('click', (e) => {
+    $('#container').addClass('works-nav-active');
+    const $this = $(e.currentTarget).attr('id');
+    const indexTotal = $('.work-single').length - 1;
+    const activeIndex = $('#work-slideshow-master .work-single.is-active').index();
+    $('.work-single').removeClass('is-active');
+    let targetIndex = '';
+    if ($this == 'works-nav-next') {
+      if (activeIndex === indexTotal) {
+        targetIndex = 0;
+      } else {
+        targetIndex = activeIndex + 1;
+      }
+    } else {
+      if (activeIndex === 0) {
+        targetIndex = indexTotal;
+      } else {
+        targetIndex = activeIndex - 1;
+      }
+    }
+    const $target = $('.work-single').eq(targetIndex);
+    $target.addClass('is-active');
+  });
+
+  // Works slideshows dots navigation
+  $('.work-slide-nav').on('click', (e) => {
+    const $this = $(e.currentTarget);
+    $this.parent().find('.work-slide-nav').removeClass('is-active');
+    $this.addClass('is-active');
+    const target = $this.index() - 1;
+    $this.closest('.work-slideshow').find('.work-slideshow-inner').removeClass('is-active');
+    const photo = $this.closest('.work-slideshow').find('.work-slideshow-inner').eq(target).find('.work-slideshow-photo').data('photo');
+    $this.closest('.work-slideshow').find('.work-slideshow-inner').eq(target).find('.work-slideshow-photo').css('background-image', `url(${photo})`);
+    $this.closest('.work-slideshow').find('.work-slideshow-inner').eq(target).addClass('is-active');
+  });
+  // Works slideshows arrows navigation
+  $('.work-slide-arrow').on('click', (e) => {
+    const $this = $(e.currentTarget);
+    const thisWay = $this.data('way')
+    const indexTotal = $this.closest('.work-slideshow').find('.work-slideshow-inner').length - 1;
+    const activeIndex = $this.parent().find('.work-slide-nav.is-active').index() - 1;
+    $this.parent().find('.work-slide-nav').removeClass('is-active');
+    let targetIndex = '';
+    if (thisWay == 'next') {
+      if (activeIndex === indexTotal) {
+        targetIndex = 0;
+      } else {
+        targetIndex = activeIndex + 1;
+      }
+    } else {
+      if (activeIndex === 0) {
+        targetIndex = indexTotal;
+      } else {
+        targetIndex = activeIndex - 1;
+      }
+    }
+    const $target = $this.parent().find('.work-slide-nav').eq(targetIndex);
+    $target.addClass('is-active');
+    $this.closest('.work-slideshow').find('.work-slideshow-inner').removeClass('is-active');
+    const photo = $this.closest('.work-slideshow').find('.work-slideshow-inner').eq(targetIndex).find('.work-slideshow-photo').data('photo');
+    $this.closest('.work-slideshow').find('.work-slideshow-inner').eq(targetIndex).find('.work-slideshow-photo').css('background-image', `url(${photo})`);
+    $this.closest('.work-slideshow').find('.work-slideshow-inner').eq(targetIndex).addClass('is-active');
+  });
 });
 $(window).load(() => {
   $('body').addClass('is-loaded');
